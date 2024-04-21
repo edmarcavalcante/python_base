cec7ac1dcee56d3bd01ede9883c421a48821b498
Edmar Almeida <eng.edmar@gmail.com>
2024-04-21 19:49:59 -0300

    Exercise resolution

--- exercise_python/02.booking_bedroom.py
+++ exercise_python/02.booking_bedroom.py
@@ -21,29 +21,66 @@ Edmar,3,12
 if another user try reserve the same bedroom, the program should show a mensage
 informing that the bedrooms is already occupied 
 """
+import sys
+import os
+
 available = {}
 with open("quartos.txt", "r") as file:
     lines = file.readlines()
-    #print(lines)
-    for line in lines[1:]:
-        #print(line)
+    for line in lines:
         Codigo, nome, preco = line.strip().split(",")
-        dic = {
-            "Codigo":Codigo,
-            "nome": nome,
-            "preco": preco
+        available[int(Codigo)] = {
+            "name":nome,
+            "preco":float(preco),
+            "Disponibilidade": True
         }
 
+print(f"Hello!! Welcome to ours hotel.")
 
-        available[Codigo] = dic
+costumer = input("What is your name? ")
+while costumer == "":
+    costumer = input("You need write your name or type 'enter' to exit: ")
+    if costumer == "":
+        break
 
-print(available)
+cod_validation = (1, 2, 3, 4)
 
-print(f"We have the follows bedrooms available:")
-for cod, d in available.items():
-    n = d["nome"]
-    p = d["preco"]
-    print(f"Código: {cod} -> {n} - R$ {p}")
+print(f"Currently our have the following bedrooms, tell me which one do you prefer? ")
+for i , ii in available.items():
+    print(f"{i} - {ii["name"]} - {ii["preco"]}")
 
-ask = input("Wich bedroom would you like? ")
+
+bedroom_number = int(input("What the number of the bedroom would you like?  "))
+
+while bedroom_number not in cod_validation:
+    print(f"Invalid code. You need type correct code (1,2,3,4): ")
+    bedroom_number = input(f"Type the code again or 'enter' to exit: ")
+    if bedroom_number == "":
+        sys.exit()
+    bedroom_number = int(bedroom_number)
+
+# Check if the bedrooms is available
+#TODO se númeo do quarto estiver no arquivo reservas, então não pode reservar
+with open ("reservas.txt", "r") as res:
+    lines = res.readlines()
+    for line in lines:
+        lista = line.strip().split(",")
+        if int(lista[1]) == int(bedroom_number):
+            print(f"Sorry! The bedrooms is occupied! ")
+            sys.exit()
+        else:
+            continue
+
+    #available[bedroom_number]["Disponibilidade"] == False
+
+
+number_day = int(input("How many day would you like reserve? "))
+
+try:
+    with open ("reservas.txt", "a") as res:
+            res.write(f"{costumer},{bedroom_number},{number_day},\n")
+except FileNotFoundError as e:
+    print(f"{str(e)}")
+    sys.exit()
+
 
